2a56e0c6490e5f95439a0cd7fb03a062366e5215
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-08-27 19:13:31 +0330

    user manager config added to setting

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "accounts",
-    "blog",
-    
+    #"blog",
+
 
 ]
 
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager config
+AUTH_USER_MODEL = "accounts.User"
