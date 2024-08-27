172e7ebfce633f16966caba1954fe312ce325313
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-08-27 20:10:20 +0330

    custom user admin created

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_superuser', 'is_active')
+    search_fields = ('email', )
+    ordering = ('email', )
+    fieldsets = (
+        ("authentication", {
+            "fields": (
+                'email',
+                'password',
+
+            ),
+            }),
+            ("permissions", {
+            "fields": (
+                'is_staff',
+                'is_active',
+                'is_superuser',
+
+            ),
+            }),
+            ("group permissions", {
+            "fields": (
+                'groups',
+                'user_permissions',
+            ),
+            }),
+            ("important dates", {
+            "fields": (
+                'last_login',
+            ),
+            }),
+    )
+    add_fieldsets = (
+        (None,{
+                "classes": ["wide"],
+                "fields": ["email",
+                           "password1", 
+                           "password2", 
+                           "is_staff", 
+                           "is_active", 
+                           "is_superuser"],
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
+
