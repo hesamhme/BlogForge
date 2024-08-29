bab9ecfe9280aa22d0cd04b3ac2db0084abfbdd7
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-08-29 13:35:03 +0330

    create view added

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -6,5 +6,5 @@ app_name = "blog"
 urlpatterns = [
     path('post/', views.PostListView.as_view(), name='post_list' ),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail' ),
-
+    path('create/', views.PostCreateViews.as_view(), name = "create_view")
 ] 
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,11 +1,22 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, Category
 
 
 class PostListView(ListView):
     model = Post
 
+
 class PostDetailView(DetailView):
     model = Post
-    
+
+
+class PostCreateViews(CreateView):
+    model = Post
+    fields = ['title', 'content', 'status', 'category', 'publish_date']
+    success_url = '/blog/post/'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
