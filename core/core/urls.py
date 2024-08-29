b9273826680fdfadbab73aee2cb44de2b0e78b06
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-08-29 14:55:08 +0330

    add django auth urls

--- core/core/urls.py
+++ core/core/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
+
 ] 
 
 # serving static and media url
