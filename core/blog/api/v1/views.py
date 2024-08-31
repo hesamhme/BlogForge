358b14f26834501d355a99658be01880673f401f
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-08-31 22:50:03 +0330

    using serializer to change data to json

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .serializers import PostSerializer
 from blog.models import Post
@@ -11,9 +13,11 @@ def post_list(request):
 
 @api_view()
 def post_detail(request, id):
-    post = Post.objects.get(pk=id)
+
+    post = get_object_or_404(Post, pk=id)
     serializer = PostSerializer(post)
     return Response(serializer.data)
+
 
 
 
