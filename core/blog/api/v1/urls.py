680380c5b067f5e5cab30ee4a6bb972331d8464c
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-09-02 22:29:26 +0330

    update urls using defualt routers

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
-app_name = "api-v1"
+router = DefaultRouter()
+router.register('post', views.PostViewSet, basename='post')
+urlpatterns = router.urls
 
-urlpatterns = [
-    # path('post/', views.post_list ) ==> this is for FBV,
-    # path('post/<int:id>/', views.post_detail )  ==> this is for FBV,
-    path('post/', views.PostList.as_view(), name='post_list' ),
-    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail' ),
-    
-] 
+# app_name = "api-v1"
+
+# urlpatterns = [
+#     # path('post/', views.post_list ) ==> this is for FBV,
+#     # path('post/<int:id>/', views.post_detail )  ==> this is for FBV,
+#     # path('post/', views.PostList.as_view(), name='post_list' ),
+#     # path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail' ),
+#     path('post/', views.PostViewSet.as_view({'get':'list'}), name='post_list'),
+#     path('post/<int:pk>/', views.PostViewSet.as_view({'get':'retrive'}), name='post_detail'), 
+   
+# ] 
+
