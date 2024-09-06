888de72ccd3c0b01e85db875e7d6171bf81ee960
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-09-06 23:33:40 +0330

    openapi.Info edited

--- core/core/urls.py
+++ core/core/urls.py
@@ -27,12 +27,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="my blog API",
       default_version='v1',
-      description="Test description",
+      description="this is doc for my blog",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(email="hesam.a.hashemi@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
