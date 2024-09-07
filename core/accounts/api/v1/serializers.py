363761260c038a57d1439afda2832aee58e3579b
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-09-08 01:17:20 +0330

    password validation added to serializers

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -4,6 +4,13 @@ from ...models import User
 
 
 class RegistrationsSerializerClass(serializers.ModelSerializer):
+    password1 = serializers.CharField(max_lengh=255, write_only = True)
     class Meta:
         model = User
-        fields = ['email', 'password']
+        fields = ['email', 'password', 'password1']
+
+        def validate(self, attrs):
+            if attrs.get('password') != attrs.get('password1'):
+                raise serializers.ValidationError({'detail': 'passwords dosenot match'})
+
+            return super().validate(attrs)
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -13,5 +13,5 @@ class RegistrationsApiView(generics.GenericAPIView):
             data = {
                 'email':serializer.validated_data['email']
             }
-        return Response(data, status=status.HTTP_201_CREATED)
-    
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
