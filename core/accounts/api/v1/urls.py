367c73546a4ef9a4a38fe9df382d8286d017e4eb
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-09-08 23:39:19 +0330

    using token for log in

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import ObtainAuthToken
 from . import views
 
 app_name = 'api-v1'
 
 urlpatterns = [
     # registration
-    path('registrations/', views.RegistrationsApiView.as_view(), name='registrations')
+    path('registrations/', views.RegistrationsApiView.as_view(), name='registrations'),
     # change password
     # reset password
     # login token
+    path('token/login/', ObtainAuthToken.as_view() , name='token-login'),
     # login jwt
 ] 
