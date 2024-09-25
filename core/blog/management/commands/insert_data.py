154833a4b9738d66148c4967f7847d82806bfd0e
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-09-25 13:18:07 +0330

    fake post with fake user ability

--- core/blog/management/commands/insert_data.py
+++ core/blog/management/commands/insert_data.py
@@ -1,10 +1,12 @@
-from io import StringIO
+import random
 from django.core.management.base import BaseCommand
 from faker import Faker
+from datetime import datetime
 from accounts.models import User, Profile
 from blog.models import Post,Category
  
-
+category_list = [
+    'test1','test2','test3','test4','test5','test6','test7']
 
 
 class Command(BaseCommand):
@@ -20,3 +22,16 @@ class Command(BaseCommand):
         profile.last_name = self.fake.last_name()
         profile.description = self.fake.paragraph(nb_sentences=5)
         profile.save()
+
+        for name in category_list:
+            Category.objects.create(name=name)
+        for _ in range(10):
+            Post.objects.create(
+                author = profile,
+                title = self.fake.paragraph(nb_sentences=1) ,
+                content = self.fake.paragraph(nb_sentences=10),
+                status = True,
+                category = Category.objects.get(name=random.choice(category_list)),
+                publish_date = datetime.now()
+
+            )
